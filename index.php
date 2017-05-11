733b1bdd28a5e88684b79b59e8e3db93bb0a0f10
akoiaka <akoi@akoi.fr>
2017-05-11 16:10:46 +0200

    jeudi

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+  include ("header.html");
+?>
+<section class="premiere">
+    <h1>MEET THE GOATS</h1>
+    <div class="imgpage1">
+      <img src="./img/calima.jpg">
+    </div>
+</section>
+<?php
+  include ("footer.html");
+?>
---
+++ page3.php
@@ -0,0 +1,22 @@
+<?php
+  include ("header.html");
+?>
+    <section class="premiere">
+      <h1>TAGGED "GOATS"</h1>
+        <div class="hr"><hr></div>
+          <div class="imgpage3">
+            <img id="img3" src="./img/sun.jpg">
+            <p>It’s been hard to keep this one a secret, but excited to finally announce that as of September 2015, all Little Seed products will be produced using 100% renewable energy - we’re going solar! We’ve long been committed to having a positive impact on the environment, so after years of hoping and planning, we’re proud to finally achieve this goal. Our hearts are happy.
+            </p>
+          <ul class="pink">
+            <li class="a">2015</li>
+            <li class="b">FARMINGHOWTO</li>
+            <li class="c">GOATS</li>
+          </ul>
+            <button type="button" name="button">READ MORE</button>
+          </div>
+    </section>
+
+<?php
+  include ("footer.html");
+?>
